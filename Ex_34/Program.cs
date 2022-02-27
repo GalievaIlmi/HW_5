0a3906652226d4ec199bac479643dd1c005eedd3
Ilmira <gal.ilmir@mail.ru>
2022-02-27 16:42:18 +0300

    add Ex_34

---
+++ Ex_34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Написать программу для замены элементов массива на противоположные.
+
+int[] array = new int[5];
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = array[i] * -1;
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
