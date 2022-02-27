44d6324b4296114fb484c5960fce982ed694dbaf
Ilmira <gal.ilmir@mail.ru>
2022-02-27 19:41:35 +0300

    add Ex_40

---
+++ Ex_38/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: Найти сумму чисел одномерного массива, стоящих на нечётной позиции.
+
+int[] array = new int[10];
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+
+{
+    int count = array.Length;
+    int sum = 0;
+    for (int i = 1; i < count; i++)
+    {
+       if(i % 2 != 0) 
+       {sum += array[i];}
+       
+    }
+    Console.WriteLine(sum);
+}
---
+++ Ex_40/Program.cs
@@ -0,0 +1,27 @@
+﻿// 40: В указанном массиве вещественных чисел найти разницу между максимальным и минимальным элементами.
+
+int num = new Random().Next(1, 10);
+Console.WriteLine($"Количество элементов массива: {num}");
+
+double[] FillArray(int num)
+{
+    double[] array = new double[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiff(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    Console.WriteLine($"Минимальное значение {arr.Min()}, максимальное значение {arr.Max()}");
+    Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+}
+PrintDiff(FillArray(num));
+
